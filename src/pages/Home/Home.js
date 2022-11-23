62ed450d76f1d546ea2fa02cc6c67471f0017b16
Shaik Shameer Basha <shaikshameer987@gmail.com>
2022-11-23 10:59:33 +0530

    created pages and components folders for ecommerce

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import Footer from "../../components/Shared/Footer/Footer"
+import Header from "../../components/Shared/Header/Header"
+
+function Home() {
+    return (
+        <>
+            <Header />
+            <h1>This is a Home Page</h1>
+            <Footer />
+        </>
+    )
+}
+
+export default Home
